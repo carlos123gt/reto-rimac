3d0a552da50405132d4b2998dd828bbe2de1747c
Carlos <n00255472@upn.pe>
2021-10-28 18:05:29 -0500

    Update

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Header from '../../components/header/header'
+import Header from '../../components/header/header';
+import ShowCase from '../../components/showcase/showcase';
 
 const Login = () => {
     return (
         <div>
             <Header></Header>
+            <ShowCase></ShowCase>
         </div>
     );
 }
